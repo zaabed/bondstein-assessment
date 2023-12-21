2a29a8bebb27f8e763c3d3dcf1d057052b9068c7
zaabed <zaaabedkhaan@gmail.com>
2023-12-21 16:52:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-emerald-600 font-extrabold text-center text-3xl mt-10">Hello Tailwind</h1>
+    </>
+  )
+}
+
+export default App
